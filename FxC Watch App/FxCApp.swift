ed08de196856007eec157e6b95179713bdaa6bbe
lgf2111 <lgf2111@gmail.com>
2023-03-09 04:29:44 +0800

    Initial Commit

---
+++ FxC Watch App/FxCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FxCApp.swift
+//  FxC Watch App
+//
+//  Created by Lee Guan Feng on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FxC_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
