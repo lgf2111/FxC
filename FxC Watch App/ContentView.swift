238621946a3e9a8bf56eab6052e2e150b65b3a4f
lgf2111 <lgf2111@gmail.com>
2023-03-09 04:34:36 +0800

    First commit

--- FxC Watch App/ContentView.swift
+++ FxC Watch App/ContentView.swift
@@ -2,22 +2,46 @@
 //  ContentView.swift
 //  FxC Watch App
 //
-//  Created by Lee Guan Feng on 9/3/23.
+//  Created by Lee Guan Feng on 22/2/23.
 //
 
 import SwiftUI
 
+struct Currency {
+    var value: String
+    var code: String
+}
+
 struct ContentView: View {
+    var rate: Float = 0.000056
+    
+    @State var startCurrency = Currency(
+        value: "1", code: "VND"
+        )
+    
+    @State var endCurrency = Currency(
+        value: "", code: "SGD"
+    )
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            FxField(value: $startCurrency.value, code: $startCurrency.code)
+            
+            Image(systemName: "arrow.up.arrow.down")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .padding(.vertical, 10)
+            
+            FxField(value: $endCurrency.value, code: $endCurrency.code, locked: true)
         }
         .padding()
+        .onAppear {
+            endCurrency.value = Float(startCurrency.value) != 1 ? String(format: "%.2f", (Float(startCurrency.value) ?? 0) * rate) : String(format: "%f", rate)
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ FxC Watch App/FxField.swift
@@ -0,0 +1,37 @@
+//
+//  FxField.swift
+//  FxC Watch App
+//
+//  Created by Lee Guan Feng on 22/2/23.
+//
+
+import SwiftUI
+import SwiftUI_Apple_Watch_Decimal_Pad
+
+struct FxField: View {
+    @Binding var value: String
+    @Binding var code: String
+    @State var locked: Bool = false
+
+    @State var presentKeyboard: Bool = false
+
+    var body: some View {
+        HStack {
+            DigiTextView(
+                placeholder: "0.00",
+                text: $value,
+                presentingModal: presentKeyboard,
+                alignment: .trailing,
+                style: .decimal
+            )
+            Text(code)
+        }
+    }
+}
+
+
+struct FxField_Previews: PreviewProvider {
+    static var previews: some View {
+        FxField(value: .constant("0.57"), code: .constant("VND"))
+    }
+}
